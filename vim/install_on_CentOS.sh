4d1d6f2cabcdae350048216234ac6302b6d64506
weimeng <m-wei@haihangyun.com>
2018-09-25 11:12:54 +0800

    merge install_on_CentOS.sh

--- vim/install_on_CentOS.sh
+++ vim/install_on_CentOS.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
+<<<<<<< HEAD
 yum install -y ctags cmake gcc-c++ python-devel ncurses-devel unzip automake pcre-devel xz-devel
+=======
+yum install -y ctags cmake gcc-c++ python-devel ncurses-devel unzip zlib-devel automake pcre-devel xz-devel
+>>>>>>> Perfect `ag` plugin
 
+# 安装Vim8
+pushd /tmp
 wget https://github.com/vim/vim/archive/master.zip
 unzip master.zip
-
-pushd ./vim-master/src
+cd ./vim-master/src
 ./configure --with-features=huge -enable-pythoninterp --with-python-config-dir=/usr/lib64/python2.7/config/
 sudo make
 sudo make install
 export PATH=/usr/local/bin:$PATH
 popd
-
-rm -rf master.zip ./vim-master
+rm -rf /tmp/master.zip /tmp/vim-master
 
 if ! ctags --list-languages | grep -qi python; then
     echo "Fail to install ctags!"
@@ -23,6 +27,22 @@ if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
     echo "Install vundle ..."
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
+
+# 安装ag
+pushd /tmp
+wget https://github.com/ggreer/the_silver_searcher/archive/master.zip
+mv master.zip ag.zip
+unzip ag.zip
+cd the_silver_searcher-master/
+./build.sh --with-png --without-zlib
+make install
+popd
+rm -rf /tmp/ag.zip /tmp/the_silver_searcher-master/ 
+
+# 配置ag.vim插件
+pushd ~/.vim/bundle
+git clone https://github.com/rking/ag.vim ag
+popd
 
 cp -v vimrc ~/.vimrc
 mkdir ~/.vim/colors
