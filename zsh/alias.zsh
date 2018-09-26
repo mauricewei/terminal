5ea9b1aa5d310712f38f6e832e292876bc668aee
weimeng <m-wei@haihangyun.com>
2018-09-26 15:56:55 +0800

    Add "tmux" alias to "tm"

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -4,6 +4,7 @@ alias cp='cp -i'
 alias ag='ag --color-match "1;31"'
 alias tailf='tail -f'
 alias proxy='env http_proxy=http://10.211.55.2:8123 https_proxy=http://10.211.55.2:8123'
+alias tm='tmux'
 
 if which vim &>/dev/null; then
     alias vi='vim'
