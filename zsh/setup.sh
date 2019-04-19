e733b92ddb4ba8f31fba4f271de9041267daec82
weimeng <mauricewei@qq.com>
2019-04-19 09:33:21 +0800

    Add zsh install

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -1,10 +1,15 @@
-#!/bin/zsh
+#!/bin/bash
 
+# 安装 zsh
+yum install zsh -y
+
+# 安装 oh my zsh
 if [[ ! -d ~/.oh-my-zsh ]]; then
     echo "Install oh-my-zsh ..."
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
+# 拷贝 zsh 环境变量和 alias 文件
 cp -v alias.zsh ~/.alias
 cp -v zshrc ~/.zshrc
 
